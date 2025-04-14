d1249828d4ee15a5ff6819660994c3646a951975
koamkun <tim.sultan20@gmail.com>
2025-04-14 13:23:14 +0600

    new class maze problems

--- src/Main.java
+++ src/Main.java
@@ -48,7 +48,33 @@ class Maze {
         }
     }
 
-    
-}
+    private boolean isInBounds(int r, int c) {
+        return r > 0 && r < rows - 1 && c > 0 && c < cols - 1;
+    }
+
+    private void carvePath(int r, int c) {
+        int[] dirs = {0, 1, 2, 3};
+        shuffle(dirs);
+
+        for (int i : dirs) {
+            int dr = dRow[i] * 2;
+            int dc = dCol[i] * 2;
+            int nr = r + dr;
+            int nc = c + dc;
+
+            if (isInBounds(nr, nc) && grid[nr][nc] == WALL) {
+                grid[r + dr / 2][c + dc / 2] = PATH;
+                grid[nr][nc] = PATH;
+                carvePath(nr, nc);
+            }
+        }
+    }
+
+
+
+    }
+
+
+
 
 
