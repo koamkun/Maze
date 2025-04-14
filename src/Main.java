1625d9e6fd0b689e4e731464a49da7ba54b54575
koamkun <tim.sultan20@gmail.com>
2025-04-14 13:20:18 +0600

    new class maze problems

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,36 @@ class Maze {
     int rows, cols;
     Random rand = new Random();
 
+
+    public Maze (int rows, int cols){
+        this.rows = (rows % 2 == 0) ? rows + 1 : rows;
+        this.cols = (cols % 2 == 0) ? cols + 1 : cols;
+        this.grid = new char[this.rows][this.cols];
+        this.visited = new boolean[this.rows][this.cols];
+    }
+
+
+
+    public void generate() {
+        for (int r = 0; r < rows; r++)
+            for (int c = 0; c < cols; c++)
+                grid[r][c] = WALL;
+
+        carvePath(1, 1);
+        grid[1][1] = PATH;
+        grid[rows - 2][cols - 2] = PATH;
+    }
+
+    private void shuffle(int[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            int index = rand.nextInt(i + 1);
+            int temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+        }
+    }
+
+
 }
 
 
